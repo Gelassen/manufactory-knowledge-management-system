99b6bdef9960e79759b7df7659d1df16351d4a8d
dkazakov <dmitrii.kazakov@gmail.com>
2024-02-22 11:45:53 +0300

    Ref #10: add backend prototype

---
+++ backend/src/main/kotlin/io/github/gelassen/manufactory_knowledge_management/ManufactoryKnowledgeManagementApplication.kt
@@ -0,0 +1,11 @@
+package io.github.gelassen.manufactory_knowledge_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManufactoryKnowledgeManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<ManufactoryKnowledgeManagementApplication>(*args)
+}
